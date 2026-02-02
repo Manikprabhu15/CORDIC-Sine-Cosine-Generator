5ff9f0eba2780233ac1016498bc7df53eb0d6ccb
Meru Manikprabhu Ratanakar <merumanikprabhu15@gmail.com>
2026-02-02 10:57:59 +0530

    Delete agent directory

--- agent/cordic_op_agent_config.sv
+++
@@ -1,7 +0,0 @@
-class cordic_op_agent_config extends uvm_object;
-	`uvm_object_utils(cordic_op_agent_config)
-	
-	uvm_active_passive_enum is_active = UVM_ACTIVE;
-	virtual cordic_if vif;
-	
-endclass : cordic_op_agent_config
